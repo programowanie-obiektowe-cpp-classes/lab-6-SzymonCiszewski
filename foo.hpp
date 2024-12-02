976f86b0a491cb9b6363dbb0dba1eba4c9134b93
Szymon Ciszewski <szymon526.ciszewski@wp.pl>
2024-12-02 21:31:25 +0100

    zejsciowka

--- foo.hpp
+++ foo.hpp
@@ -9,9 +9,9 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-    std::for_each(people.begin(), people.end(), [](auto& c) { c->birthday(); });
+    std::for_each(people.begin(), people.end(), [](auto& c) { c.birthday(); });
     std::transform(people.begin(), people.end(), retval.begin(), [&](auto& c) {
-        if (c->isMonster)
+        if (c.isMonster())
             retval.push_back('n');
         else
             retval.push_back('y');
