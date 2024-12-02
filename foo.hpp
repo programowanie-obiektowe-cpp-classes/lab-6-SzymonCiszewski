198c93144ca6dc0a3e500ad7d9be23396d233a4b
SzymonCiszewski <127044576+SzymonCiszewski@users.noreply.github.com>
2024-12-02 22:46:29 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -1,22 +1,23 @@
-pragma once
+#pragma once
 
 #include "Human.hpp"
 
 #include <algorithm>
 #include <list>
 #include <vector>
-
+    
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
     std::for_each(people.begin(), people.end(), [](auto& c) { c.birthday(); });
     std::transform(people.begin(), people.end(), retval.begin(), [&](auto& c) {
         if (c.isMonster())
-            retval.push_back('n');
+            return 'n';
         else
-            retval.push_back('y');
+            return 'y';
     });
     // Twoja implementacja tutaj
 
     return retval;
 }
+
